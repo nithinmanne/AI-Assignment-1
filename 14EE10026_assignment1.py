895492de721685d00f418afdcb2b45b107b7eb4c
Naganithin Manne <nithinmanne@gmail.com>
2017-08-15 14:46:17 +0530

    First working astar

--- 14EE10026_assignment1.py
+++ 14EE10026_assignment1.py
@@ -1,10 +1,15 @@
+import heapq
+from copy import deepcopy as copy
 
 class State(list):
     '''Class for holding the state as a list of lists of NxN'''
 
-    def __init__(self, *state):
+    def __init__(self, *state, blank = None):
         list.__init__(self, *state)
         self.N = len(*state)
+        self.level = 0
+        if(blank == None):
+            self.blank = self.get_blank()
 
     def __str__(self):
         state_str = ""
@@ -13,7 +18,54 @@ class State(list):
                     state_str += str(j)
         return state_str
 
+    def get_blank(self):
+        for i in range(self.N):
+            for j in range(self.N):
+                if(self[i][j]==0): return (i, j)
+
+    def push_blank_up(self):
+        if self.blank[0]==0:
+            return None
+        nstate = State(copy(self), blank = 1)
+        nstate.level = self.level + 1
+        nstate[self.blank[0]][self.blank[1]] = self[self.blank[0]-1][self.blank[1]]
+        nstate.blank = (self.blank[0] - 1, self.blank[1])
+        nstate[nstate.blank[0]][nstate.blank[1]] = 0
+        return nstate
+
+    def push_blank_down(self):
+        if self.blank[0]==self.N-1:
+            return None
+        nstate = State(copy(self), blank = 1)
+        nstate.level = self.level + 1
+        nstate[self.blank[0]][self.blank[1]] = self[self.blank[0]+1][self.blank[1]]
+        nstate.blank = (self.blank[0] + 1, self.blank[1])
+        nstate[nstate.blank[0]][nstate.blank[1]] = 0
+        return nstate
+
+    def push_blank_left(self):
+        if self.blank[1]==0:
+            return None
+        nstate = State(copy(self), blank = 1)
+        nstate.level = self.level + 1
+        nstate[self.blank[0]][self.blank[1]] = self[self.blank[0]][self.blank[1]-1]
+        nstate.blank = (self.blank[0], self.blank[1] - 1)
+        nstate[nstate.blank[0]][nstate.blank[1]] = 0
+        return nstate
+
+    def push_blank_right(self):
+        if self.blank[1]==self.N-1:
+            return None
+        nstate = State(copy(self), blank = 1)
+        nstate.level = self.level + 1
+        nstate[self.blank[0]][self.blank[1]] = self[self.blank[0]][self.blank[1]+1]
+        nstate.blank = (self.blank[0], self.blank[1] + 1)
+        nstate[nstate.blank[0]][nstate.blank[1]] = 0
+        return nstate
+
+
 '''Decorator for the Heuristic Function'''
+
 def heuristic_function(heuristic):
     def wrapper(goal):
         def h_fn(state):
@@ -83,4 +135,42 @@ def linear_conflict(state, goal):
                     break
 
 
-hasattr(zero, 'heuristic_function') and getattr(zero, 'heuristic_function')
+def astar(start, goal, heuristic = None, f_n = 'g(state) + h(state)'):
+    if heuristic == None:
+        heuristic = zero
+    if not(hasattr(heuristic, 'heuristic_function') and getattr(heuristic, 'heuristic_function')):
+        raise ValueError("Given function needs to be decorated with @heuristic_function")
+    h = heuristic(goal)
+    def g(state):
+        return state.level
+    heap = []
+    nodes = 0
+    visited = set()
+    state = start
+    visited.add(str(state))
+    heapq.heappush(heap, (eval(f_n), state))
+    while(len(heap)>0):
+        f_nv, ostate = heapq.heappop(heap)
+        nodes += 1
+        if(str(ostate) == str(goal)):
+            return ostate, nodes
+        state = ostate.push_blank_up()
+        if (state!=None and str(state) not in visited):
+            visited.add(str(state))
+            heapq.heappush(heap, (eval(f_n), state))
+        state = ostate.push_blank_down()
+        if (state!=None and str(state) not in visited):
+            visited.add(str(state))
+            heapq.heappush(heap, (eval(f_n), state))
+        state = ostate.push_blank_left()
+        if (state!=None and str(state) not in visited):
+            visited.add(str(state))
+            heapq.heappush(heap, (eval(f_n), state))
+        state = ostate.push_blank_right()
+        if (state!=None and str(state) not in visited):
+            visited.add(str(state))
+            heapq.heappush(heap, (eval(f_n), state))
+
+
+goal = State([[1,2,3],[4,5,6],[7,8,0]])
+start = State([[1,2,3],[4,5,6],[7,0,8]])
